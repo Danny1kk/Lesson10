85d4ef76d3b363c2b4bfc5396b8e9bb54c875674
Danny1kk <danny.koshik@gmail.com>
2024-08-15 20:47:24 +0300

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -1,7 +1,8 @@
 list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 a = 0
 while a < len(list):
-    print(list[a])
-    a += 1
     if list[a] < 0:
         break
+    elif list[a] > 0:
+        print(list[a])
+    a += 1
